5a90cfb202ea828701a9c4df6c8af8d70ea4588c
dahye <bbngsk@gmail.com>
2023-08-01 16:18:59 +0900

    상세화면 구현 완료

--- StudyWeek3/CustomTableViewController.swift
+++ StudyWeek3/CustomTableViewController.swift
@@ -9,11 +9,30 @@ import UIKit
 
 class CustomTableViewController: UITableViewController {
 
-    let todo = ToDoInformation()
+    @IBOutlet var searchBar: UISearchBar!
+
+    var todo = ToDoInformation() {
+        didSet { //변수가 달라짐을 감지
+            print("갱신")
+            tableView.reloadData()
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.rowHeight = 80
+        searchBar.placeholder = "할 일을 입력해보세요"
+        searchBar.searchTextField.addTarget(self, action: #selector(searchBarReturnTapped), for: .editingDidEndOnExit)
+    }
+
+    @objc func searchBarReturnTapped() {
+        //ToDo 항목을 list에 추가
+        let data = ToDo(main: searchBar.text!, sub: "23.08.01", like: false, done: false)
+        todo.list.insert(data, at: 0)
+        //UX
+        searchBar.text = ""
+        //갱신
+        //tableView.reloadData()
     }
 
     //1.
@@ -29,12 +48,33 @@ class CustomTableViewController: UITableViewController {
 
         cell.configureCell(row: row)
 
+        cell.likeButton.tag = indexPath.row
+        cell.likeButton.addTarget(self, action: #selector(likeButtonClicked), for: .touchUpInside)
+
         return cell
+    }
+
+    @objc
+    func likeButtonClicked(_ sender: UIButton) {
+        print("clicked \(sender.tag)")
+        todo.list[sender.tag].like.toggle()
+        //tableView.reloadData()
     }
 
     //3.
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         print(indexPath)
+    }
+
+    //삭제
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        //제거 -> 갱신
+        todo.list.remove(at: indexPath.row)
+        //tableView.reloadData()
     }
 
 }
